de0539473d957f64fcc4a342c94672ffea11b314
Tietohallinto <tietohallinto@BC-minimac060.local>
2020-09-10 10:55:26 +0300

    add clock

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-var HOURHAND = document.getElementsById('hour');
-var MINUTEHAND = document.querySelector('.minute');
-var SECONDHAND = document.getElementsByName('second');
+var HOURHAND = document.getElementById("hour");
+var MINUTEHAND = document.querySelector(".minute");
+var SECONDHAND = document.getElementsByName("second");
+
+function time() {
+  var now = new Date();
+  var hr = now.getHours();
+  var minutes = now.getMinutes();
+  var sec = now.getSeconds();
+  var tick = 360 / 60;
+}
